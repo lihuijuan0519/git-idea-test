4c05ec1abbd85a66cb2bdc7c7ce62cab8726fd5a
lihuijuan <2911132438@qq.com>
2022-03-31 15:59:53 +0800

    第2次提交本地库  修改了testcontroller

--- src/main/java/cn/tt/git/controller/TestController.java
+++ src/main/java/cn/tt/git/controller/TestController.java
@@ -1,4 +1,9 @@
 package cn.tt.git.controller;
 
+import org.apache.xpath.operations.String;
+
 public class TestController {
+    public static void main(String[] args){
+        System.out.println("hello git!");
+    }
 }
